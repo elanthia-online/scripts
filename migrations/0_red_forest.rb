c87e07c8b35944ef48fa7e80cd008b7397c68551
ondreian <benjamin.clos@gmail.com>
2018-05-27 08:13:51 -0400

    add red forest migration;

--- migrations/0_red_forest.rb
+++ migrations/0_red_forest.rb
@@ -1,4 +1,11 @@
 migrate :aggressive_npc do
-  insert(:name, 
-         "winged viper")
+  insert(:name, "winged viper")
+  insert(:name, "Illvari pixie")
+  insert(:name, "Illvari sprite")
+  insert(:name, "direbear")
+  insert(:name, "monstrous direwolf")
+  insert(:name, "warped tree spirit")
+  insert(:name, "treekin warrior")
+  insert(:name, "treekin druid")
+  insert(:name, "treekin sapling")
 end
