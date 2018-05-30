1c3e66755f6fc7e5979e804ca93efd33f34ae54e
Charlie Sanders <sanderscharlie@gmail.com>
2018-05-30 18:12:18 -0500

    gameobj-data: junk spec

---
+++ spec/gameobj-data/junk_spec.rb
@@ -0,0 +1,72 @@
+require 'lich/gameobj'
+require 'spec/factories'
+
+describe GameObj do
+  describe "junk" do
+    [
+      %{smooth stone},
+      %{spirit shard},
+      %{small block of salt},
+      %{table leg},
+      %{some tree bark},
+      %{piece of iron},
+      %{moldy bone},
+      %{chipped brick},
+      %{some tattered cloth},
+    ].each do |junk_name|
+      it "recognizes #{junk_name} as junk" do
+        junk = GameObjFactory.item_from_name(junk_name)
+        expect(junk.type).to eq "junk"
+        expect(junk.sellable).to be nil
+      end
+    end
+
+    describe "junk armor" do
+      [
+        %{steel shield},
+        %{scratched steel helm},
+      ].each do |junk_name|
+        it "recognizes #{junk_name} as junk" do
+          junk = GameObjFactory.item_from_name(junk_name)
+          expect(junk.type).to include "junk"
+        end
+
+        xit "recognizes #{junk_name} is NOT armor" do
+          junk = GameObjFactory.item_from_name(junk_name)
+          expect(junk.type).to_not include "armor"
+          expect(junk.sellable).to be nil
+        end
+      end
+    end
+
+    describe "randomized junk names from treasure system" do
+      junk_nouns = %w[coin cup doorknob fork horseshoe nail spoon]
+      junk_descriptors = %w[bent corroded dented polished rusty scratched shiny tarnished]
+
+      junk_nouns.product(junk_descriptors).each do |noun, desc|
+        junk_name = "#{desc} #{noun}"
+
+        it "recognizes #{junk_name} as junk" do
+          junk = GameObjFactory.item_from_name(junk_name)
+          expect(junk.type).to eq "junk"
+          expect(junk.sellable).to be nil
+        end
+      end
+
+      junk_jewelry = %w[anklet bracelet earring medallion plate ring]
+      junk_jewelry.product(junk_descriptors).each do |noun, desc|
+        junk_name = "#{desc} #{noun}"
+
+        it "recognizes #{junk_name} as junk" do
+          junk = GameObjFactory.item_from_name(junk_name)
+          expect(junk.type).to eq "junk"
+        end
+
+        xit "recognizes #{junk_name} as unsellable" do
+          junk = GameObjFactory.item_from_name(junk_name)
+          expect(junk.sellable).to be nil
+        end
+      end
+    end
+  end
+end
