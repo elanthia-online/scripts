080545129c98db4f7f3c6e71ea4a1517442835a1
Charlie Sanders <sanderscharlie@gmail.com>
2018-08-14 01:31:24 -0500

    gameobj-data: expect non treasure system nouns to not be sellable

--- spec/gameobj-data/clothing_spec.rb
+++ spec/gameobj-data/clothing_spec.rb
@@ -3,86 +3,91 @@ require 'spec/factories'
 
 describe GameObj do
   describe "clothing" do
-    [
-      %{armband},
-      %{backpack},
-      %{bag},
-      %{bandana},
-      %{belt},
-      %{blouse},
-      %{bodice},
-      %{bonnet},
-      %{boots},
-      %{bow},
-      %{breeches},
-      %{cap},
-      %{cape},
-      %{chemise},
-      %{cloak},
-      %{coat},
-      %{dress},
-      %{frock},
-      %{gloves},
-      %{gown},
-      %{greatcloak},
-      %{harness},
-      %{hat},
-      %{hood},
-      %{jacket},
-      %{kerchief},
-      %{kilt},
-      %{kirtle},
-      %{knapsack},
-      %{leggings},
-      %{longcloak},
-      %{longcoat},
-      %{mantle},
-      %{overcoat},
-      %{pack},
-      %{pants},
-      %{pouch},
-      %{purse},
-      %{quiver},
-      %{ribbon},
-      %{robe},
-      %{robes},
-      %{sack},
-      %{sandals},
-      %{sash},
-      %{satchel},
-      %{scabbard},
-      %{scarf},
-      %{shawl},
-      %{sheath},
-      %{shirt},
-      %{shoes},
-      %{skirt},
-      %{slippers},
-      %{snood},
-      %{socks},
-      %{surcoat},
-      %{tabard},
-      %{trousers},
-      %{tunic},
-      %{vest},
-    ].each do |clothing_name|
-      it "recognizes #{clothing_name} as clothing" do
-        clothing = GameObjFactory.item_from_name(clothing_name)
-        expect(clothing.type).to include "clothing"
-        expect(clothing.sellable.to_s).to include "pawnshop"
+    describe "randomized treasure system drop nouns" do
+      [
+        %{armband},
+        %{backpack},
+        %{bag},
+        %{bandana},
+        %{belt},
+        %{blouse},
+        %{bodice},
+        %{bonnet},
+        %{boots},
+        %{bow},
+        %{breeches},
+        %{cap},
+        %{cape},
+        %{chemise},
+        %{cloak},
+        %{coat},
+        %{dress},
+        %{frock},
+        %{gloves},
+        %{gown},
+        %{greatcloak},
+        %{harness},
+        %{hat},
+        %{hood},
+        %{jacket},
+        %{kerchief},
+        %{kilt},
+        %{kirtle},
+        %{knapsack},
+        %{leggings},
+        %{longcloak},
+        %{longcoat},
+        %{mantle},
+        %{overcoat},
+        %{pack},
+        %{pants},
+        %{pouch},
+        %{purse},
+        %{quiver},
+        %{ribbon},
+        %{robe},
+        %{robes},
+        %{sack},
+        %{sandals},
+        %{sash},
+        %{satchel},
+        %{scabbard},
+        %{scarf},
+        %{shawl},
+        %{sheath},
+        %{shirt},
+        %{shoes},
+        %{skirt},
+        %{slippers},
+        %{snood},
+        %{socks},
+        %{surcoat},
+        %{tabard},
+        %{trousers},
+        %{tunic},
+        %{vest},
+      ].each do |clothing_name|
+        it "recognizes #{clothing_name} as clothing" do
+          clothing = GameObjFactory.item_from_name(clothing_name)
+          expect(clothing.type).to include "clothing"
+          expect(clothing.sellable.to_s).to include "pawnshop"
+        end
       end
     end
 
-    [
-      %{kit},
-    ].each do |clothing_name|
-      it "recognizes #{clothing_name} as clothing" do
-        clothing = GameObjFactory.item_from_name(clothing_name)
-        expect(clothing.type).to include "clothing"
-      end
+    describe "clothing not usually found in randomized treasure" do
+      [
+        %{bloodstained leather survival kit},
+      ].each do |clothing_name|
+        it "recognizes #{clothing_name} as clothing" do
+          clothing = GameObjFactory.item_from_name(clothing_name)
+          expect(clothing.type).to include "clothing"
+        end
 
-      xit "recognizes #{clothing_name} is sellable at the pawnshop" do
-        expect(clothing.sellable.to_s).to include "pawnshop"
+        it "recognizes #{clothing_name} is NOT sellable at the pawnshop" do
+          clothing = GameObjFactory.item_from_name(clothing_name)
+          expect(clothing.sellable.to_s).to_not include "pawnshop"
+        end
       end
     end
 
