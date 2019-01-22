0396affb2ccf0e585a3ed032e6f42bc066e1d206
Benjamin Clos <benjamin.clos@gmail.com>
2019-01-22 15:18:24 -0500

    add aggressive npcs;

---
+++ type_data/migrations/7_sos.rb
@@ -0,0 +1,8 @@
+migrate :aggressive_npc do
+  insert(:name, %[shambling lurk])
+  insert(:name, %[patchwork flesh monstrosity])
+  insert(:name, %[lithe veiled sentinel])
+  insert(:name, %[pale scaled shaper])
+  insert(:name, %[deathsworn fanatic])
+  insert(:name, %[white sidewinder])
+end
