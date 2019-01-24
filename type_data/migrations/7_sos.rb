e3b3c7b5b51727686514bdd7a433964afc7782b6
Benjamin Clos <benjamin.clos+github@gmail.com>
2019-01-24 17:05:48 -0500

    add SoS definitions; (#99)
    * add aggressive npcs;
    
    * [migration/sos] add undead; add gems;
    
    * [migration/sos] remove duplicate wulfenite entry;
    
    * [migration/sos] remove more duplicates;

---
+++ type_data/migrations/7_sos.rb
@@ -0,0 +1,17 @@
+migrate :aggressive_npc do
+  insert(:name, %[lithe veiled sentinel])
+  insert(:name, %[pale scaled shaper])
+  insert(:name, %[deathsworn fanatic])
+  insert(:name, %[white sidewinder])
+end
+
+migrate :aggressive_npc, :undead do
+  insert(:name, %[shambling lurk])
+  insert(:name, %[patchwork flesh monstrosity])
+end
+
+migrate :gem, :gemshop do
+  insert(:name, %[oblong blue goldstone])
+  insert(:name, %[dull grey crystal])
+  insert(:name, %[pink salt crystal])
+end
