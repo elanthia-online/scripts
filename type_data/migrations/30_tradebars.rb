b58c92ee6f850dd4935838c1c9f7a57d10ebac90
FFNG <zedwordmail@gmail.com>
2020-07-10 16:34:24 +0000

    Add boon/miniboss/glam creature type

---
+++ type_data/migrations/30_tradebars.rb
@@ -0,0 +1,12 @@
+migrate :gemshop, :valuable do
+  insert(:name, %{small copper tradebar})
+  insert(:name, %{small bronze tradebar})
+  insert(:name, %{small silver tradebar})
+  insert(:name, %{small gold tradebar})
+  insert(:name, %{small platinum tradebar})
+  insert(:name, %{large copper tradebar})
+  insert(:name, %{large bronze tradebar})
+  insert(:name, %{large silver tradebar})
+  insert(:name, %{large gold tradebar})
+  insert(:name, %{large platinum tradebar})
+end
