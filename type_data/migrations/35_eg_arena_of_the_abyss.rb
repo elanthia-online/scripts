5d7a8d4e071d36e9755a3eb9bb6acfd30c383cbc
Charlie Sanders <sanderscharlie@gmail.com>
2020-10-11 12:13:31 -0500

    gameobj: add Ebon Gate's Arena of the Abyss creatures

---
+++ type_data/migrations/35_eg_arena_of_the_abyss.rb
@@ -0,0 +1,15 @@
+migrate :aggressive_npc, :undead do
+  insert(:name, %{ancient ghoul horror})
+  insert(:name, %{contorted ghoul master})
+  insert(:name, %{ethereal battlemaster})
+  insert(:name, %{fanged nebulous figure})
+  insert(:name, %{fiery-eyed eidolon})
+  insert(:name, %{hunchbacked arch wight})
+  insert(:name, %{inky-wisped gaunt figure})
+  insert(:name, %{massive-armed hulking dybbuk})
+  insert(:name, %{mindless abomination})
+  insert(:name, %{phantasmal combatant})
+  insert(:name, %{rotting dwarven guard})
+  insert(:name, %{sore-covered zombie})
+  insert(:name, %{spectral dwarven miner})
+end
