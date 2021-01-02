079bd4f053a330fedd442a819bc26b92c25b6519
Matthew Sawyer <msawyer@redhat.com>
2021-01-02 16:13:55 -0500

    Added Exceptions for 'modwir' boxes to remove them from uncommon

--- type_data/migrations/13_use_names_not_nouns_for_loot_boxes.rb
+++ type_data/migrations/13_use_names_not_nouns_for_loot_boxes.rb
@@ -17,3 +17,12 @@ migrate :box do
   #long
   insert(:name, %{(?:austere|brass-inlaid|crude|gilded|ornate|scorched) (?:carved modwir|cracked|deeply-scored|delicate|red lacquered|stained) (?:box|chest|coffer|strongbox|trunk|case) (?:covered with tiny worm holes|decorated with bits of colorful glass|engraved with the image of a pile of gems|held together with ragged iron straps|painted with a resplendent sun on the top|swathed in rust-red symbols|with a broken and rusted chain attached|with frayed ropes for handles|with tiny clawed feet|wrapped in red silk|wrapped in green silk|wrapped in black silk)})
 end
+
+migrate :uncommon do
+  create_key(:prefix)
+  insert(:prefix, %{shifting})
+
+  insert(:exclude, %{(?:(?:badly damaged|engraved|enruned|iron-bound|plain|rotting|scratched|simple|sturdy|weathered) )?(?:modwir) (?:box|chest|coffer|strongbox|trunk)})
+  insert(:exclude, %{(?:carved modwir) (?:box|chest|coffer|strongbox|trunk|case)})
+  insert(:exclude, %{(?:austere|brass-inlaid|crude|gilded|ornate|scorched) (?:carved modwir) (?:box|chest|coffer|strongbox|trunk|case) (?:covered with tiny worm holes|decorated with bits of colorful glass|engraved with the image of a pile of gems|held together with ragged iron straps|painted with a resplendent sun on the top|swathed in rust-red symbols|with a broken and rusted chain attached|with frayed ropes for handles|with tiny clawed feet|wrapped in red silk|wrapped in green silk|wrapped in black silk)})
+end
