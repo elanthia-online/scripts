c3b1793d53141d4be3b0158893c048141688bf0b
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2022-05-23 20:37:51 -0400

    [gameobj] Forgotten Valley
    add the forgotten valley NPCs

---
+++ type_data/migrations/47_forgotten-valley.rb
@@ -0,0 +1,9 @@
+migrate :aggressive_npc do
+  insert(:name, %{shan bard})
+  insert(:name, %{shan bardess})
+  insert(:name, %{shan empath})
+  insert(:name, %{shan rogue})
+  insert(:name, %{shan shaman})
+  insert(:name, %{shan sorcerer})
+  insert(:name, %{shan sorceress})
+end
