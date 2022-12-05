5f4d38dcd3e3c3e51a5e7d3b9ce485766eaa0c33
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2022-12-05 11:48:12 -0500

    [gameobj] mother-of-pearl correction
    mother-of-pearl is not a gem and is not purifieable and is actually listed as "MISC" by ingames FLAG sort method and not under "GEMS". Needs to move to `valuable` from `gem`

--- type_data/migrations/52_missing_valuables.rb
+++ type_data/migrations/52_missing_valuables.rb
@@ -3,11 +3,13 @@ migrate :gemshop do
 end
 
 migrate :gem do
+  delete(:noun, %{mother\-of\-pearl})
   insert(:name, %{piece of petrified maoral})
   insert(:name, %{piece of petrified thanot})
   insert(:name, %{piece of rosespar})
 end
 
 migrate :valuable do
+  insert(:noun, %{mother\-of\-pearl})
   delete(:name, %{piece of rosespar})
 end
