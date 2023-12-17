f50fd38b2f86a9c1b32bbe5ee11a2519cbf99915
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-12-17 18:26:21 -0500

    [gameobj] Stormpeak
    initial creation of migration file for stormpeak area

---
+++ type_data/migrations/57_stormpeak.rb
@@ -0,0 +1,10 @@
+migrate :aggressive_npc do
+  insert(:name, %{water hound})
+  insert(:name, %{vapor hound})
+  insert(:name, %{storm hound})
+  insert(:name, %{swirling spectre})
+  insert(:name, %{lightning fiend})
+  insert(:name, %{titan stormcaller})
+  insert(:name, %{tempest tyrant})
+  insert(:name, %{veiki herald})
+end
