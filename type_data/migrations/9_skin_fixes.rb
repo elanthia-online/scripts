835e21a3df3c58a6b9ad7ec26a1e65bc066bd71b
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-01-21 11:40:27 -0500

    [gameobj-data.xml] fix wind witch nose bundles
    Wind witch skins are a "crooked witch nose" and are recognized as skins.  When bundled, they become "a bundle of witch noses" and are no longer recognized as skins.

--- type_data/migrations/9_skin_fixes.rb
+++ type_data/migrations/9_skin_fixes.rb
@@ -7,6 +7,7 @@ migrate :skin, :furrier do
   insert(:name, %{brown boar hide})
   insert(:name, %{moor eagle talon})
   insert(:name, %{mountain snowcat pelt})
+  insert(:name, %{(?:crooked )?witch nose})     # from wind witch
 end
 
 migrate :skin, :furrier do
