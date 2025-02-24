6cda7fbe1f2bbaa0b849db42df474f0644e6ddb3
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-02-23 19:53:01 -0500

    rubocop

--- type_data/migrations/64_lock_and_key.rb
+++ type_data/migrations/64_lock_and_key.rb
@@ -1,6 +1,6 @@
 create_table "lockandkey", keys: [:name]
 
 migrate "lockandkey", "uncommon" do
-  insert(:name, %{vibrant rainbow-hued key}) 
-  insert(:name, %{vibrant rainbow-hued lock}) 
+  insert(:name, %{vibrant rainbow-hued key})
+  insert(:name, %{vibrant rainbow-hued lock})
 end
