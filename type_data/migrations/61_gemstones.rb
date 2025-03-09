abfa1e4cb9ac0a9af33bffb6d94d1a6d79cade31
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-03-08 21:09:27 -0500

    [gameobj-data.xml] additional gemstone ascension updates

--- type_data/migrations/61_gemstones.rb
+++ type_data/migrations/61_gemstones.rb
@@ -1,5 +1,11 @@
 create_table "ascension:jewel", keys: [:noun]
+create_table "ascension:quest", keys: [:name]
+
 
 migrate "ascension:jewel" do
   insert(:noun, %[jewel])
+end
+
+migrate "ascension:quest", "uncommon" do
+  insert(:name, %{silver-veined black draconic idol})
 end
