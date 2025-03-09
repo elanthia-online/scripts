8521d63b9e03553d39174feaf43cc2c2716f31e4
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-03-08 22:51:45 -0500

    fix: add additional quest components

--- type_data/migrations/61_gemstones.rb
+++ type_data/migrations/61_gemstones.rb
@@ -1,10 +1,19 @@
-create_table "ascension:jewel", keys: [:noun]
+create_table "ascension:jewel", keys: [:noun, :exclude]
 create_table "ascension:quest", keys: [:name]
 
 migrate "ascension:jewel" do
   insert(:noun, %[jewel])
+  insert(:exclude, %{iron black jewel})
 end
 
 migrate "ascension:quest", :uncommon do
   insert(:name, %{silver-veined black draconic idol})
+end
+
+migrate "ascension:quest" do
+  insert(:name, %{small cloth bundle})
+  insert(:name, %{ancient crumbling gemstone})
+  insert(:name, %{tattered parchment note})
+  insert(:name, %{tear-shaped iridescent gemstone})
+  insert(:name, %{iron black jewel})
 end
