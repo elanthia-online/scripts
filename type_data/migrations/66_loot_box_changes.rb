d423aba937771eec9c593bd051db876acf4d0c71
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2026-01-16 20:59:57 -0500

    fix: add cooper

--- type_data/migrations/66_loot_box_changes.rb
+++ type_data/migrations/66_loot_box_changes.rb
@@ -1,8 +1,8 @@
 migrate :box do
   create_key(:name)
-  insert(:name, %{(?:gold-trimmed|copper-(?:edged|trimmed)|rune-incised|waterlogged|blackened|lacquered|scratched|battered|riveted|enruned|charred|painted|rotting|scuffed|jeweled|scarred|ornate|singed|fluted|banded|sturdy|plain )?(?:cherrywood|white oak|mahogany|hickory|bronze|modwir|walnut|silver|maoral|wooden|cedar|maple|steel|thanot|monir|tanik|brass|iron|gold|fel) (?:strongbox|coffer|chest|trunk|box)})
+  insert(:name, %{(?:gold-trimmed|copper-(?:edged|trimmed)|rune-incised|waterlogged|blackened|lacquered|scratched|battered|riveted|enruned|charred|painted|rotting|scuffed|jeweled|scarred|ornate|singed|fluted|banded|sturdy|plain )?(?:cherrywood|white oak|mahogany|hickory|bronze|modwir|walnut|silver|maoral|cooper|wooden|cedar|maple|steel|thanot|monir|tanik|brass|iron|gold|fel) (?:strongbox|coffer|chest|trunk|box)})
 end
 
 migrate :uncommon do
-  insert(:exclude, %{(?:gold-trimmed|copper-(?:edged|trimmed)|rune-incised|waterlogged|blackened|lacquered|scratched|battered|riveted|enruned|charred|painted|rotting|scuffed|jeweled|scarred|ornate|singed|fluted|banded|sturdy|plain )?(?:cherrywood|white oak|mahogany|hickory|bronze|modwir|walnut|silver|maoral|wooden|cedar|maple|steel|thanot|monir|tanik|brass|iron|gold|fel) (?:strongbox|coffer|chest|trunk|box)})
+  insert(:exclude, %{(?:gold-trimmed|copper-(?:edged|trimmed)|rune-incised|waterlogged|blackened|lacquered|scratched|battered|riveted|enruned|charred|painted|rotting|scuffed|jeweled|scarred|ornate|singed|fluted|banded|sturdy|plain )?(?:cherrywood|white oak|mahogany|hickory|bronze|modwir|walnut|silver|maoral|cooper|wooden|cedar|maple|steel|thanot|monir|tanik|brass|iron|gold|fel) (?:strongbox|coffer|chest|trunk|box)})
 end
