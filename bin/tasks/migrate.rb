20ab807bdc4c16ad198cdf2de7ddd8b06cbcf916
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2026-01-31 11:32:55 -0500

    fix: initial rspec update

--- bin/tasks/migrate.rb
+++ bin/tasks/migrate.rb
@@ -2,10 +2,13 @@
 ## load all assets
 ##
 require_relative("../../lib/migration.rb")
+puts '1'
 ##
 ## do eeet
 ##
 Migration.run(
   migrations: Dir[Migration.cwd("type_data", "migrations", "**", "*.rb")],
   tables:     Dir[Migration.cwd("type_data", "tables", "**", "*.yaml")],
-  dist:       Migration.cwd("dist"))
+  dist:       Migration.cwd("dist")
+)
+puts '2'
