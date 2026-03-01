62f115d68ea415319cb5452260b481a01943e5b9
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2026-03-01 08:46:45 -0500

    fix: add various event box notes

--- type_data/migrations/69_note_corrections.rb
+++ type_data/migrations/69_note_corrections.rb
@@ -12,4 +12,12 @@ migrate :note do
   insert(:name, %{City-States promissory note})
   insert(:name, %{Vornavis promissory note})
   insert(:name, %{Mist Harbor promissory note})
+
+  # various event box reward notes
+  insert(:name, %{bloodstained promissory note})
+  insert(:name, %{damp promissory note})
+  insert(:name, %{dirty promissory note})
+  insert(:name, %{old promissory note})
+  insert(:name, %{smudged promissory note})
+  insert(:name, %{stained promissory note})
 end
