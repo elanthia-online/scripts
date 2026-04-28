edd266e06b4a49a684017f26219db6e038fe3861
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2026-04-28 09:47:58 -0400

    fix(gameobj-data.xml): add jewelry and clothing nouns to migrations
    Added new nouns for jewelry and clothing migrations.

---
+++ type_data/migrations/70_transmogrification_jewelry_clothing.rb
@@ -0,0 +1,35 @@
+migrate :gemshop, :pawnshop, :jewelry do
+  insert(:noun, %{armbands})
+  insert(:noun, %{bangle})
+  insert(:noun, %{beads})
+  insert(:noun, %{bracers})
+  insert(:noun, %{collar})
+  insert(:noun, %{comb})
+  insert(:noun, %{corsage})
+  insert(:noun, %{choker})
+  insert(:noun, %{ear studs?})
+  insert(:noun, %{fichu})
+  insert(:noun, %{hairclip})
+  insert(:noun, %{hairpin})
+  insert(:noun, %{hairsticks?})
+  insert(:noun, %{necklet})
+end
+
+migrate :pawnshop, :clothing do
+  insert(:noun, %{case})
+  insert(:noun, %{cinch})
+  insert(:noun, %{clogs})
+  insert(:noun, %{coif})
+  insert(:noun, %{cowl})
+  insert(:noun, %{jerkin})
+  insert(:noun, %{haversack})
+  insert(:noun, %{singlet})
+  insert(:noun, %{shift})
+  insert(:noun, %{smock})
+  insert(:noun, %{tam'o'shanter})
+end
+
+migrate :gemshop, :jewelry do
+  insert(:noun, %{periapt})
+  insert(:exclude, %{small bone periapt})
+end
