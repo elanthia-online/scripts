30769d1b4f282826e054f6795e67cf938c598d0a
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2026-04-29 08:45:27 -0400

    Add 'buskins' to pawnshop clothing nouns

--- type_data/migrations/70_transmogrification_jewelry_clothing.rb
+++ type_data/migrations/70_transmogrification_jewelry_clothing.rb
@@ -19,6 +19,7 @@ migrate :gemshop, :pawnshop, :jewelry do
 end
 
 migrate :pawnshop, :clothing do
+  insert(:noun, %{buskins})
   # insert(:noun, %{case}) # conflicts with base "box" type
   insert(:noun, %{cinch})
   insert(:noun, %{clogs})
